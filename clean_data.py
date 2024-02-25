62c1187a6dde4e4dade28f8f46ebcbe473b69fd5
Sean Duffy <146782486+sean-r-duffy@users.noreply.github.com>
2024-02-25 14:00:05 -0500

    Create hierarchical and kmeans.ipynb

--- clean_data.py
+++ clean_data.py
@@ -17,3 +17,16 @@ def clean(df, region_type='zip', yr_pcts=(1, 2, 3)):
         df = df.drop(columns=['RegionID', 'SizeRankZip', 'RegionName'])
     
     return df
+
+def model_features(df, add=None):
+    column_selection = ['ZORI 2023', 'ZHVI 2023', 'ZHVF 2025-01-31', 'Transit Score', 'NatWalkInd', '3yr pct change ZHVI',
+                        '3yr pct change ZORI', '3yr pct change Sale Listings', '3yr pct change Sales', '3yr pct change Days on Market',
+                        '3yr pct change New Construction']
+    
+    # Adding any features not included by default
+    if add is not None:
+        for x in add:
+            if x not in column_selection:
+                column_selection.append(x)
+    
+    return(df[column_selection])
